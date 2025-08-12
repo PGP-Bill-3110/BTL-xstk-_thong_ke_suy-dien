fbdf7373641588acd3062c2224ffeb4aa3e08ab1
PGP-Bill-3110 <160414335+PGP-Bill-3110@users.noreply.github.com>
2025-08-12 15:05:57 +0700

    Add files via upload
    Sửa lại file thống kê suy diễn

--- tk suy dien.R
+++ tk suy dien.R
@@ -23,7 +23,7 @@ head(main_GPU)
 dim(main_GPU)
 ###################################################################################
 
-#5.1
+#5.1 bt 1 mau  
 
 # Loc du lieu, lay nha san xuat Nvidia
 library(dplyr)
@@ -36,22 +36,29 @@ qqnorm(nvidia_data$Memory_Bandwidth, main = "QQ-Plot of Memory Bandwidth (NVIDIA
 qqline(nvidia_data$Memory_Bandwidth, col = "red", lwd = 2)
       # =>suy ra Memory_Bandwidth có tuân theo pp chuuan ko
 
+#option 2
+qqnorm(nvidia_data, main = "QQ-Plot")
+qqline(nvidia_data, col = "red", lwd = 2)
+
+
 
 # Kiem tra phan phoi chuan voi Shapiro-Wilk Test
 shapiro_result <- shapiro.test(nvidia_data$Memory_Bandwidth)$p.value
-print(shapiro_result)
+print(shapiro_result)  # so voi 0.05 de ket luan
 
 # Tinh toan ket qua kiem dinh t-test
 t_statistic = t.test(nvidia_data$Memory_Bandwidth, mu = 150, alternative = "greater")$
   statistic
-t_statistic
+t_statistic    
 
-# Xac dinh mien bac bo Z_alpha voi muc y nghia 5%
-print(qnorm(0.95))
+# slide chuong 5, trang 319  Z_qs  (ko pp chuẩn)
+
+# Xac dinh mien bac bo Z_alpha voi muc y nghia 5% 
+print(qnorm(0.95))    #(z-alpah; +vc)
 
 ################################################################################
 
-#5.2
+#5.2 bt 2 mau
 
 GPUnodedicated <- subset(main_GPU, main_GPU$Dedicated == "No")  # Nhóm GPU không dedicated (tích hợp)
 GPUdedicated   <- subset(main_GPU, main_GPU$Dedicated == "Yes") # Nhóm GPU dedicated (card rời)
@@ -84,15 +91,21 @@ shapiro.test(GPUnodedicated$Memory_Bandwidth)
 shapiro.test(GPUdedicated$Memory_Bandwidth)
 
 
-# gt kiem dinh
-Zo = (xtb1-xtb2)/sqrt(s1^2/n1 + s2^2/n2)
- print(Zo)
- 
 # mien bb
  qnorm(p=0.05,lower.tail = FALSE)
 
+ 
+ #t_test   
+ t_result <- t.test(GPUnodedicated$Memory_Bandwidth,
+                    GPUdedicated$Memory_Bandwidth,
+                    var.equal = FALSE,      # Không giả định phương sai bằng nhau
+                    alternative = "two.sided",
+                    conf.level = 0.95)
+ 
+ print(t_result)
+ 
  ###################################################################
- #5.3
+ #5.3   anova 1 yeu to
  
  #Kiem tra dieu kien ppc
  
@@ -103,7 +116,7 @@ Zo = (xtb1-xtb2)/sqrt(s1^2/n1 + s2^2/n2)
  
  library(nortest)
  av_residual<-rstandard(aov(Memory_Bandwidth~Manufacturer))
- shapiro.test(av_residual)
+ shapiro.test(av_residual)  #so voi 0.05
  
  #lay mau
  frequency_AMD <- main_GPU$Memory_Bandwidth[grep("AMD", main_GPU$Manufacturer, 
@@ -125,7 +138,7 @@ Zo = (xtb1-xtb2)/sqrt(s1^2/n1 + s2^2/n2)
    Q3 <- quantile(x, 0.75)
    IQR_value <- IQR(x)
    lower_limit <- Q1 - 1.5 * IQR_value
-   upper_limit <- Q3 + 1.5 * IQR_value
+   upper_limit <- Q3 + 1.5 * IQR_value   #slide chuong 4, trang 247
    return(x[x >= lower_limit & x <= upper_limit])
  }
  
@@ -149,7 +162,7 @@ Phuong_sai = c(sd(frequency_AMD), sd(frequency_ATI),
 Kich_thuoc_mau = c(length(frequency_AMD), length(frequency_ATI), 
                    length(frequency_Intel), length(frequency_Nvidia))
  )
-
+#ct slide chương 4 trang 245
 
 #mien bb
  qf(1- 0.05, 4- 1,length(frequency_AMD) 
@@ -181,52 +194,54 @@ Kich_thuoc_mau = c(length(frequency_AMD), length(frequency_ATI),
  ################################################################### 
  
  # 5.4
- model_1 <- lm(Memory_Bandwidth ~ Memory_Speed + L2_Cache + Memory_Bus + Shader + 
-                 Dedicated + Manufacturer, data = main_GPU)
- summary(model_1)
+ # HỒI QUY TUYẾN TÍNH ĐƠN
  
- # bo shader
- model_2 <- lm(Memory_Bandwidth ~ Memory_Speed + L2_Cache + Memory_Bus + 
-                 Dedicated + Manufacturer, data = main_GPU)
- summary(model_2)
- 
- # do thi kt gia dinh 
- par(mfrow = c(2, 2))
- plot(model_2)
- 
- # chi tiet tung bieu do
- par(mfrow = c(1, 1))
- #bđ 1
- plot(model_2, which = 1)  # Residuals vs Fitted
+ model_speed <- lm(Memory_Bandwidth ~ Memory_Speed, data = main_GPU)
+ summary(model_speed)
  
  
-#bđ 2
- plot(model_2, which = 2)  #  Q-Q residuals
+ # Vẽ biểu đồ scatter plot + đường hồi quy
+ plot(main_GPU$Memory_Speed, main_GPU$Memory_Bandwidth,
+      main = "Hồi quy tuyến tính đơn:\n Memory_Bandwidth ~ Memory_Speed",
+      xlab = "Memory Speed (MHz)",
+      ylab = "Memory Bandwidth (GB/s)",
+      pch = 16, col = "blue")
  
- # sharpio wilk test phan du mo hinh 2
- model_residuals <- residuals(model_2) #Dua phan du cua mo hinh vao mo bien khac
- shapiro.test(model_residuals) #Thuc hien phep thu len bien
- 
- #bđ 3
- plot(model_2, which = 3)  # Scale-Location
- 
- #bđ 4
- plot(model_2, which = 5)  # Residuals vs Leverage
+ # Thêm đường hồi quy vào biểu đồ
+ abline(model_speed, col = "red", lwd = 2)
 
- #loai bo diem vuot qua cook's distance
  
- cooks_distance <- cooks.distance(model_2)
- #Tao nguong loai bo
-  threshold <- 4 / nrow(main_GPU) 
-  #Xac dinh cac diem co anh huong cao
-high_influence_points <- which(cooks_distance > threshold)
+ #loai bỏ outliner
+ 
+ # Huấn luyện mô hình ban đầu
+ model_speed <- lm(Memory_Bandwidth ~ Memory_Speed, data = main_GPU)
+ 
+ # Tính Cook's distance
+ cooksD <- cooks.distance(model_speed)
+ 
+ # Thiết lập ngưỡng loại bỏ: 4/n là ngưỡng phổ biến
+ threshold <- 4 / nrow(main_GPU)
+ 
+ # Lấy các điểm có ảnh hưởng lớn
+ influential_points <- which(cooksD > threshold)
+ 
+ # Xóa các điểm này khỏi dữ liệu
+ main_GPU_cleaned <- main_GPU[-influential_points, ]
+ 
+ # Huấn luyện lại mô hình sau khi loại bỏ
+ model_cleaned <- lm(Memory_Bandwidth ~ Memory_Speed, data = main_GPU_cleaned)
+ 
+ # Tóm tắt mô hình mới
+ summary(model_cleaned)
+ 
+ # Vẽ mô hình hồi quy sau khi làm sạch dữ liệu
+ plot(main_GPU_cleaned$Memory_Speed, main_GPU_cleaned$Memory_Bandwidth,
+      main = "Hồi quy tuyến tính đơn sau khi loại bỏ outlier:\nMemory_Bandwidth ~ Memory_Speed",
+      xlab = "Memory Speed (MHz)",
+      ylab = "Memory Bandwidth (GB/s)",
+      pch = 20, col = "blue")
+ 
+ # Thêm đường hồi quy
+ abline(model_cleaned, col = "red", lwd = 2)
 
-#Loai bo cac diem co anh huong cao va tao lai mo hinh
-data_filtered <- main_GPU[-high_influence_points, ]
-filltered_model_2 <- lm(Memory_Bandwidth ~ Memory_Speed + L2_Cache + Memory_Bus +
-                             Manufacturer, data = data_filtered) 
-plot(filltered_model_2) 
  
-# cong da tuyen
-library(car)
-vif(model_2)
